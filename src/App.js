815ab0be051fc3623148b6c4e515c6d244f17ce1
nora-sch <nora.schumann.development@gmail.com>
2023-07-05 15:41:27 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./Docker-Logo-2013.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        This is Dockerized React App
+      </header>
+    </div>
+  );
+}
+
+export default App;
